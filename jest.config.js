328991a09c7d913641035fa1abc6a85211920e25
SlavaNordic <265652304+SlavaNordic@users.noreply.github.com>
2026-09-11 14:31:45 +0200

    Update shared to v260 (#561)
    * Update shared to v259; Remove mocks of TextEncoder and TextDecoder
    
    * Update shared to v260

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,10 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
-const path = require('path');
 
 const config =
     require('@nordicsemiconductor/pc-nrfconnect-shared/config/jest.config')([
         'serialport',
     ]);
-
-config.setupFilesAfterEnv.push(
-    path.join(__dirname, 'src', 'common', 'polyfillJest.js'),
-);
 
 module.exports = config;
